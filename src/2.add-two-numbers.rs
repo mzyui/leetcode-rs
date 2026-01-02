53164faa45ededbb40a7be15a5558667d70735d7
mzyui <xnver404@gmail.com>
2026-01-02 09:15:16 +0700

    feat(algorithms): add Two Sum and Add Two Numbers solutions

---
+++ src/2.add-two-numbers.rs
@@ -0,0 +1,82 @@
+// Category: algorithms
+// Level: Medium
+// Percent: 47.55582%
+
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+//
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//
+//
+// Example 1:
+//
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+//
+//
+// Example 2:
+//
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+//
+//
+// Example 3:
+//
+// Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+// Output: [8,9,9,9,0,0,0,1]
+//
+//
+//
+// Constraints:
+//
+//
+// 	The number of nodes in each linked list is in the range [1, 100].
+// 	0 <= Node.val <= 9
+// 	It is guaranteed that the list represents a number that does not have leading zeros.
+//
+
+// Definition for singly-linked list.
+//
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//     pub val: i32,
+//     pub next: Option<Box<ListNode>>,
+// }
+//
+// impl ListNode {
+//     #[inline]
+//     fn new(val: i32) -> Self {
+//         ListNode { next: None, val }
+//     }
+// }
+
+impl Solution {
+    pub fn add_two_numbers(
+        mut l1: Option<Box<ListNode>>,
+        mut l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut dummy = Box::new(ListNode::new(0));
+        let mut tail = &mut dummy;
+        let mut carry = 0;
+
+        while l1.is_some() || l2.is_some() || carry != 0 {
+            let mut sum = carry;
+
+            if let Some(mut n1) = l1.take() {
+                sum += n1.val;
+                l1 = n1.next.take();
+            }
+
+            if let Some(mut n2) = l2.take() {
+                sum += n2.val;
+                l2 = n2.next.take();
+            }
+
+            carry = sum / 10;
+            tail.next = Some(Box::new(ListNode::new(sum % 10)));
+            tail = tail.next.as_mut().unwrap();
+        }
+
+        dummy.next
+    }
+}
